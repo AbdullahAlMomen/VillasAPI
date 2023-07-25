e71a0765fcb764c7ee1754cbb90d6efd60f9f82c
momen <abdullah@celimited.com>
2023-07-25 10:58:02 +0600

    #feat: basic get and project initialization done

---
+++ VillasAPICore/Controllers/VillaAPIController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VillasAPICore.DataStore;
+using VillasAPICore.Models.DTO;
+using VillasAPICore.Models.Entity;
+
+namespace VillasAPICore.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet("GetVillas")]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaDataStore.VillaDTOs;
+            
+        }
+
+
+        [HttpGet("GelVilla/{id:int}")]
+        public VillaDTO GetVillas(int id)
+        {
+            return VillaDataStore.VillaDTOs.FirstOrDefault(x => x.Id == id); ;
+
+        }
+
+
+    }
+}
---
+++ VillasAPICore/DataStore/VillaDataStore.cs
@@ -0,0 +1,13 @@
+﻿using VillasAPICore.Models.DTO;
+
+namespace VillasAPICore.DataStore
+{
+    public static class VillaDataStore
+    {
+        public static List<VillaDTO> VillaDTOs = new List<VillaDTO>
+        {
+            new VillaDTO { Id = 1,Name = "Matiranga"},
+             new VillaDTO { Id = 2,Name = "Bonsai"},
+        };
+    }
+}
